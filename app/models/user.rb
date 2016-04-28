594e2aca1d42835ba133560810ec6eee701ab715
Shanika <sjbutts@gmail.com>
2016-04-28 15:08:57 -0400

    Add comments to post and fix edit views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :posts
+  belongs_to :user 
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
